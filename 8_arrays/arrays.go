f2c907c35fb030c625a69d16a7b2638294b66285
SandipKurmi <sandipkurmi0@gmail.com>
2024-09-28 17:17:47 +0530

    "Added examples of direct array initialization, string array, and 2D array in main function."

--- 8_arrays/arrays.go
+++ 8_arrays/arrays.go
@@ -13,7 +13,6 @@ func main() {
 	//array length
 	fmt.Println(len(nums))
 	
-
 	// bool array
 	var vals [4]bool
 	vals[2] = true
@@ -23,4 +22,16 @@ func main() {
 	var valsstr [4]string
 	valsstr[2] = "sandip"
 	fmt.Println(valsstr)
+
+	// direct assign values
+	var ags [4]int = [4]int{1,2,3,4}
+	fmt.Println(ags)	
+
+	var names  [4]string =  [4]string{"sandip", "uday", "sachin", "kumar"}
+	fmt.Println(names)
+
+	// 2d arrays
+	var arr2d [3][3]int = [3][3]int{{1,2,3},{4,5,6},{7,8,9}}
+	fmt.Println(arr2d)
+
 }
